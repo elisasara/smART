c660b5b5696803bec509e8ab64c7a7ea6bfbd881
Elisa Urda <urda.elisa@gmail.com>
2018-04-29 16:11:10 -0400

    successfully feeding api images to cards

--- smART_PMA/components/api.js
+++
@@ -1,8 +0,0 @@
-var apiCall = {
-    getImages(){
-        var url = "https://hackathon.philamuseum.org/api/v0/collection/object?query=34120&api_token=8dP6ovY0qpRjI7v4Ljs23RykaOWWzbT15i8kPr2in3bPwgNadjK06287MjUa";
-        return fetch(url).then((res) => res.json());
-    }
-}
-
-module.exports = apiCall;
